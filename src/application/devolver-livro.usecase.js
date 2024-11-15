a37843e285a867d639d2e9172612f6aa6447c4b1
Samuel Luis dos Santos <mucasantos@gmail.com>
2024-11-15 17:29:02 -0300

    teste atraso da devolucao

--- src/application/devolver-livro.usecase.js
+++ src/application/devolver-livro.usecase.js
@@ -2,12 +2,19 @@ const { Either } = require('../shared');
 
 module.exports = function devolverLivroUseCase({ emprestimoRepository }) {
   return async function ({ emprestimo_id, data_devolucao }) {
-    await emprestimoRepository.devolver({
+    const { data_retorno } = await emprestimoRepository.devolver({
       emprestimo_id,
       data_devolucao
     });
 
-    const verificarMulta = 'Multa por atraso: R$ 0';
+    const verificarDataRetorno =
+      new Date(data_retorno).getTime() > new Date(data_devolucao).getTime();
+
+    console.log(verificarDataRetorno);
+
+    const verificarMulta = verificarDataRetorno
+      ? 'Multa por atraso: R$ 10,00'
+      : 'Multa por atraso: R$ 0';
 
     return Either.Right(verificarMulta);
   };
--- src/application/devolver-livro.usecase.spec.js
+++ src/application/devolver-livro.usecase.spec.js
@@ -5,6 +5,9 @@ describe('Devolver Livro UseCase', () => {
     devolver: jest.fn()
   };
   test('Deve ser possivel devolver um livro sem multa', async () => {
+    emprestimoRepository.devolver.mockResolvedValue({
+      data_retorno: new Date('2024-02-06')
+    });
     const devolverLivroDTO = {
       emprestimo_id: 'qualquer_id',
       data_devolucao: new Date('2024-02-06')
@@ -14,6 +17,23 @@ describe('Devolver Livro UseCase', () => {
     const output = await sut(devolverLivroDTO);
 
     expect(output.right).toBe('Multa por atraso: R$ 0');
+    expect(emprestimoRepository.devolver).toHaveBeenCalledWith(devolverLivroDTO);
+    expect(emprestimoRepository.devolver).toHaveBeenCalledTimes(1);
+  });
+
+  test('Deve devolver um livro com multa', async () => {
+    emprestimoRepository.devolver.mockResolvedValue({
+      data_retorno: new Date('2024-02-07')
+    });
+    const devolverLivroDTO = {
+      emprestimo_id: 'qualquer_id',
+      data_devolucao: new Date('2024-02-06')
+    };
+
+    const sut = devolverLivroUsecase({ emprestimoRepository });
+    const output = await sut(devolverLivroDTO);
+
+    expect(output.right).toBe('Multa por atraso: R$ 10,00');
     expect(emprestimoRepository.devolver).toHaveBeenCalledWith(devolverLivroDTO);
     expect(emprestimoRepository.devolver).toHaveBeenCalledTimes(1);
   });
