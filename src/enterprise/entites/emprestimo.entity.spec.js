cbee85f20627c19e0b1b4d559ffc1f46b3e62e98
Samuel Luis dos Santos <mucasantos@gmail.com>
2024-11-15 19:26:40 -0300

    criar os testes da entity

---
+++ src/enterprise/entites/emprestimo.entity.spec.js
@@ -0,0 +1,21 @@
+const emprestimoEntity = require('./emprestimo.entity');
+
+describe('Emprestimos Entity', function () {
+  test('CalcularMulta - sem atraso', () => {
+    const resultado = emprestimoEntity.calcularMulta({
+      data_devolucao: '2024-02-16',
+      data_retorno: '2024-02-16'
+    });
+
+    expect(resultado).toBe('Multa por atraso: R$ 0');
+  });
+
+  test('CalcularMulta - com atraso', () => {
+    const resultado = emprestimoEntity.calcularMulta({
+      data_devolucao: '2024-02-17',
+      data_retorno: '2024-02-16'
+    });
+
+    expect(resultado).toBe('Multa por atraso: R$ 10,00');
+  });
+});
