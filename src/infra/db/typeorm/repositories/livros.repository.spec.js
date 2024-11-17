de0250d314243980718869d1cdfa108751ef7a59
Samuel Luis dos Santos <mucasantos@gmail.com>
2024-11-16 22:51:55 -0300

    criar o repo de livros e o metodo de cadastrar

---
+++ src/infra/db/typeorm/repositories/livros.repository.spec.js
@@ -0,0 +1,22 @@
+const { livrosRepository } = require('./livros.repository');
+
+describe('Livros Repository TypeORM', function () {
+  let sut;
+
+  beforeAll(function () {
+    sut = livrosRepository();
+  });
+  const livroDTO = {
+    nome: 'nome_valido',
+    quantidade: 3,
+    autor: 'autor_valido',
+    genero: 'genero_valido',
+    ISBN: 'ISBN_valido'
+  };
+
+  test('Deve retornar void ao criar um livro', async () => {
+    const livroCriado = await sut.cadastrar(livroDTO);
+
+    expect(livroCriado).toBeUndefined();
+  });
+});
--- src/infra/db/typeorm/setup.js
+++ src/infra/db/typeorm/setup.js
@@ -1,11 +1,12 @@
 const typeorm = require('typeorm');
+const LivroEntityTypeorm = require('./entities/Livro.entity-typeorm');
 
 const typeormServer = new typeorm.DataSource({
   type: 'sqlite',
   database: 'db.sqlite',
   synchronize: true,
   dropSchema: true,
-  entities: [require('./entities/Usuario.entity-typeorm')]
+  entities: [require('./entities/Usuario.entity-typeorm'), LivroEntityTypeorm]
 });
 
 module.exports = { typeormServer };
