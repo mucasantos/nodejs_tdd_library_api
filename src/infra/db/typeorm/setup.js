a0ba367d58132cb9498b197300f2d87c12c73eff
Samuel Luis dos Santos <mucasantos@gmail.com>
2024-11-16 23:37:54 -0300

    criar entidade de emprestimos do typeorm

--- src/infra/db/typeorm/setup.js
+++ src/infra/db/typeorm/setup.js
@@ -1,12 +1,17 @@
 const typeorm = require('typeorm');
 const LivroEntityTypeorm = require('./entities/Livro.entity-typeorm');
+const EmprestimoEntityTypeorm = require('./entities/Emprestimo.entity-typeorm');
 
 const typeormServer = new typeorm.DataSource({
   type: 'sqlite',
   database: 'db.sqlite',
   synchronize: true,
   dropSchema: true,
-  entities: [require('./entities/Usuario.entity-typeorm'), LivroEntityTypeorm]
+  entities: [
+    require('./entities/Usuario.entity-typeorm'),
+    LivroEntityTypeorm,
+    EmprestimoEntityTypeorm
+  ]
 });
 
 module.exports = { typeormServer };
