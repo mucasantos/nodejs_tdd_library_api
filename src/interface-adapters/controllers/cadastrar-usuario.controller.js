ec6e589a80bd9fe131811dc8053935766c66c783
Samuel Luis dos Santos <mucasantos@gmail.com>
2024-11-18 18:52:19 -0300

    adicionado o zod para validar cadastro

--- src/interface-adapters/controllers/cadastrar-usuario.controller.js
+++ src/interface-adapters/controllers/cadastrar-usuario.controller.js
@@ -1,12 +1,43 @@
+const { AppError } = require('../../shared');
 const httpResponse = require('../../shared/helpers/http.response');
+
+const { z } = require('zod');
+
+const zodValidator = z.object({
+  nome_completo: z.string({
+    required_error: 'Nome completo é obrigatório!'
+  }),
+  CPF: z
+    .string({
+      required_error: ' CPF é obrigatório'
+    })
+    .refine((value) => /ˆ([0-9]{3}\.?[0-9]{3}\.[0-9]{3}\-?[0-9]{2})$/.test(value)),
+  endereco: z.string({
+    required_error: 'Endereço obrigatório'
+  }),
+  endereco: z.string({
+    required_error: 'Endereço obrigatório'
+  }),
+  telefone: z.string({
+    required_error: 'telefone obrigatório'
+  }),
+  email: z
+    .string({
+      required_error: 'email obrigatório'
+    })
+    .email({
+      message: 'Email deve ser válido'
+    })
+});
 
 module.exports = async function cadastrarUsuarioController({
   cadastrarUsuarioUseCase,
   httpRequest
 }) {
-  console.log(httpRequest);
-
-  const { nome_completo, CPF, endereco, telefone, email } = httpRequest.body;
+  if (!cadastrarUsuarioUseCase || !httpRequest || httpResponse) {
+    throw new AppError(AppError.dependenciasAusentes);
+  }
+  const { nome_completo, CPF, endereco, telefone, email } = zodValidator.parse(httpRequest.body);
   const output = await cadastrarUsuarioUseCase({
     nome_completo,
     CPF,
