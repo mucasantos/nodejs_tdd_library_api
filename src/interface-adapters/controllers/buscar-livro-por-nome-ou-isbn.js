fe2e1e5bd11bedb20146c1e4658bde9bc943d98a
Samuel Luis dos Santos <mucasantos@gmail.com>
2024-11-20 19:56:28 -0300

    cria controller buscarLivroPornomeOuISBN

---
+++ src/interface-adapters/controllers/buscar-livro-por-nome-ou-isbn.js
@@ -0,0 +1,22 @@
+const { z } = require('zod');
+const httpResponse = require('../../shared/helpers/http.response');
+
+const zodValidator = z.object({
+  valor: z.string({
+    required_error: 'Valor é obrigatorio'
+  })
+});
+
+module.exports = async function buscarLivroPorNomeOuISBNController({
+  buscarLivroPorNomeOuISBNUseCase,
+  httpResquest
+}) {
+  const { valor } = zodValidator.parse(httpResquest.query);
+
+  const output = await buscarLivroPorNomeOuISBNUseCase({ valor });
+
+  return output.fold(
+    (error) => httpResponse(400, error.message),
+    (livros) => httpResponse(200, livros)
+  );
+};
