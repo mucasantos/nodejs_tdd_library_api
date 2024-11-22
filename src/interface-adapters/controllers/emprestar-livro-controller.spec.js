3d7a7cc12bfb0a8e26be1151803163906cd86c24
Samuel Luis dos Santos <mucasantos@gmail.com>
2024-11-21 23:33:42 -0300

    cria o test que verifica se o controller retonar o Either.Left corretamente

--- src/interface-adapters/controllers/emprestar-livro-controller.spec.js
+++ src/interface-adapters/controllers/emprestar-livro-controller.spec.js
@@ -28,4 +28,28 @@ describe('Emprestar livro Controller', function () {
     });
     expect(emprestarLivroUseCase).toHaveBeenCalledTimes(1);
   });
+  test('Deve ser possivel emprestar um livro', async () => {
+    emprestarLivroUseCase.mockResolvedValue(Either.Left({ message: 'validacao_invalida' }));
+    const httpRequest = {
+      body: {
+        livro_id: 1,
+        usuario_id: 1,
+        data_saida: '2024-02-03',
+        data_retorno: '2024-02-15'
+      }
+    };
+    const response = await emprestarLivroController({
+      emprestarLivroUseCase,
+      httpRequest
+    });
+
+    expect(response).toEqual(httpResponse(400, 'validacao_invalida'));
+    expect(emprestarLivroUseCase).toHaveBeenLastCalledWith({
+      livro_id: 1,
+      usuario_id: 1,
+      data_saida: expect.any(Date),
+      data_retorno: expect.any(Date)
+    });
+    expect(emprestarLivroUseCase).toHaveBeenCalledTimes(1);
+  });
 });
