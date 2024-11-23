6e283375f8eaaa50fd6926c422233e71dfe44805
Samuel Luis dos Santos <mucasantos@gmail.com>
2024-11-23 10:48:20 -0300

    cria o arquivo de livros outes

--- src/main/api/routes/index.js
+++ src/main/api/routes/index.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
 const { usuariosRoutes } = require('./usuario.routes');
+const { livrosRoutes } = require('./livros.routes');
 
 const routes = Router();
 routes.use('/usuarios', usuariosRoutes);
+routes.use('/livros', livrosRoutes);
 
 module.exports = { routes };
