727cf80658d66cd4af1b7269d879b33c0efef606
Samuel Luis dos Santos <mucasantos@gmail.com>
2024-11-23 17:51:00 -0300

    criar o compose e a rota de buscar livro por nome e isbn

---
+++ src/main/api/composers/buscar-livro-por-nome-ou-isbn.compose.js
@@ -0,0 +1,17 @@
+const buscarLivrosPorNomeOuIsbnUsecase = require('../../../application/buscar-livros-por-nome-ou-isbn.usecase');
+const { livrosRepository } = require('../../../infra/db/typeorm/repositories/livros.repository');
+const buscarLivroPorNomeOuIsbnController = require('../../../interface-adapters/controllers/buscar-livro-por-nome-ou-isbn');
+
+module.exports = async function buscarLivroPorNomeOuISBNCompose(httpRequest) {
+  const livrosRepositoryFn = livrosRepository();
+
+  const buscarLivroPorNomeOuISBNUseCaseFn = buscarLivrosPorNomeOuIsbnUsecase({
+    livrosRepository: livrosRepositoryFn
+  });
+  const controller = buscarLivroPorNomeOuIsbnController({
+    buscarLivroPorNomeOuISBNUseCase: buscarLivroPorNomeOuISBNUseCaseFn,
+    httpRequest
+  });
+
+  return controller;
+};
--- src/main/api/routes/livros.routes.js
+++ src/main/api/routes/livros.routes.js
@@ -1,5 +1,6 @@
-const { Router, response } = require('express');
+const { Router } = require('express');
 const cadastrarLivroCompose = require('../composers/cadastrar-livro.compose');
+const buscarLivroPorNomeOuIsbnCompose = require('../composers/buscar-livro-por-nome-ou-isbn.compose');
 
 const livrosRoutes = Router();
 
@@ -10,6 +11,14 @@ livrosRoutes.post('/', async (request, response) => {
 
   const { statusCode, body } = await cadastrarLivroCompose(httpRequest);
   return response.status(statusCode).json(body);
+});
+
+livrosRoutes.get('/', async (request, response) => {
+  const httpRequest = {
+    query: request.query
+  };
+
+  const { statusCode, body } = await buscarLivroPorNomeOuIsbnCompose(request);
 });
 
 module.exports = { livrosRoutes };
